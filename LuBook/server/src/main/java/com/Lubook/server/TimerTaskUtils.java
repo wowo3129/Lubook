5d568a3e383c6c89c2ee6ed43995159ca5d0f502
yangdong <1094035520@qq.com>
2017-09-15 09:58:47 +0800

    dev: 解决Error:Execution failed for task ‘:app:mergeDebugResources’方案：aaptOptions.cruncherEnabled aaptOptions.useNewCruncher  false

--- LuBook/server/src/main/java/com/Lubook/server/TimerTaskUtils.java
+++ LuBook/server/src/main/java/com/Lubook/server/TimerTaskUtils.java
@@ -18,20 +18,4 @@ public class TimerTaskUtils {
         }, 200, 1000);/*task,firsttime,period 每间隔1s重复一次*/
     }
 }
-/*
 
-//以下是几种常用调度task的方法：
-timer.schedule(task, time);
-// time为Date类型：在指定时间执行一次（不周期）。
-
-timer.schedule(task, firstTime, period);
-// firstTime为Date类型,period为long
-// 从firstTime时刻开始，每隔period毫秒执行一次。
-
-timer.schedule(task, delay) // delay 为long类型：从现在起过delay毫秒之后执行一次（不周期）
-
-timer.schedule(task, delay, period)
-// delay为long,period为long：从现在起过delay毫秒以后，每隔period
-// 毫秒执行一次。
-
-*/
